b5550c6b7024b708d874bb24ac9e13fdddbc20a6
Garhaughawout <108907661+Garhaughawout@users.noreply.github.com>
2024-03-19 14:24:16 -0600

    All Tests passing
    All Tests are now passing and finalized.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+var employee = {
+    name: 'Sam',
+    streetAddress: '11 Broadway'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return employee = {...employee, [key]: value}
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    delete employee[key]
+    employee[key] = value
+    console.log(employee)
+    return employee 
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    return employee = {...employee, [key]: undefined}
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
+
+console.log(employee)
